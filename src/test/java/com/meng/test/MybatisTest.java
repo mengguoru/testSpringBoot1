18961f6c397a02a534664c716537a7fdcafafb45
mengguoru <2506839494@qq.com>
2019-12-26 15:31:20 +0800

    测试类

--- src/test/java/com/meng/test/MybatisTest.java
+++ src/test/java/com/meng/test/MybatisTest.java
@@ -6,13 +6,23 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"classpath:SqlMapConfig.xml"})
+@SpringBootTest
 public class MybatisTest {
-    public static void main(String[] args) throws IOException {
+
+    @Test
+    public void testFindAll() throws  Exception{
         InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(in);
@@ -23,4 +33,8 @@ public class MybatisTest {
         for(User user:users)
             System.out.println(user);
     }
+
+//    public static void main(String[] args) throws IOException {
+//
+//    }
 }
